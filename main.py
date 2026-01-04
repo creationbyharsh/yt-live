14845eaa623a5bb84aaeead415c1835669f741fa
creationbyharsh <creationbyharsh@gmail.com>
2026-01-04 19:40:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import time
 
-STREAM_KEY = "YAHAN_APNI_STREAM_KEY_PASTE_KARO"
+STREAM_KEY = "drwc-z7mz-pm90-1h0h-bjqd"
 
 cmd = f"ffmpeg -re -stream_loop -1 -i video.mp4 -c:v copy -c:a aac -b:a 160k -f flv rtmp://a.rtmp.youtube.com/live2/{STREAM_KEY}"
 

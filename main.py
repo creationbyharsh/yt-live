29e4b015b4f6dfcc19aa187bb82bfca47a73e711
creationbyharsh <creationbyharsh@gmail.com>
2026-01-04 20:01:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import time
 
 STREAM_KEY = "drwc-z7mz-pm90-1h0h-bjqd"
 
-cmd = f"ffmpeg -re -stream_loop -1 -i video.mp4 -c:v copy -c:a aac -b:a 160k -f flv rtmp://a.rtmp.youtube.com/live2/{drwc-z7mz-pm90-1h0h-bjqd}"
+command = "ffmpeg -re -stream_loop -1 -i video.mp4 -c:v copy -c:a aac -b:a 160k -f flv rtmp://a.rtmp.youtube.com/live2/" + drwc-z7mz-pm90-1h0h-bjqd
 
 while True:
-    os.system(cmd)
+    os.system(command)
     time.sleep(5)

bf1cd5fb1e8a0ec585cf479d11d907097d5fd8d7
creationbyharsh <creationbyharsh@gmail.com>
2026-01-04 19:23:57 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import os
+import time
+
+STREAM_KEY = "YAHAN_APNI_STREAM_KEY_PASTE_KARO"
+
+cmd = f"ffmpeg -re -stream_loop -1 -i video.mp4 -c:v copy -c:a aac -b:a 160k -f flv rtmp://a.rtmp.youtube.com/live2/{STREAM_KEY}"
+
+while True:
+    os.system(cmd)
+    time.sleep(5)
